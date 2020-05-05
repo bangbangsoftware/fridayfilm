042b65b385864195c97feb6579f9ec52f7222d3c
Mick Carter <cick.marter@gmail.com>
2020-05-05 17:19:00 +0100

    Formatted HTML and it's my week

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 const crew = [];
-crew.push("Paxo");
-crew.push("Bear");
-crew.push("Karl");
 crew.push("Mick");
+crew.push("Claire");
+crew.push("Paxo");
+crew.push("Karl");
 
-crew.forEach((member,index) => console.log(index+". "+ member));
-
+crew.forEach((member, index) => console.log(index + ". " + member));
 
 const year = [];
 
 let crewNo = 1;
 
-for(let x = 0; x < 53; x++){
-    year[x] = crew[crewNo];
-    crewNo = crewNo + 1;
-    if (crewNo +1 > crew.length){
-        crewNo = 0; 
-    }
-    console.log(x+". "+year[x]);
+for (let x = 0; x < 53; x++) {
+  year[x] = crew[crewNo];
+  crewNo = crewNo + 1;
+  if (crewNo + 1 > crew.length) {
+    crewNo = 0;
+  }
+  console.log(x + ". " + year[x]);
 }
 
-const getWeekNumber = date => {
-    const copiedDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
-    copiedDate.setUTCDate(copiedDate.getUTCDate() + 4 - (copiedDate.getUTCDay()||7));
-    var yearStart = new Date(Date.UTC(copiedDate.getUTCFullYear(),0,1));
-    var weekNo = Math.ceil(( ( (copiedDate - yearStart) / 86400000) + 1)/7);
-    return weekNo;
-}
+const getWeekNumber = (date) => {
+  const copiedDate = new Date(
+    Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
+  );
+  copiedDate.setUTCDate(
+    copiedDate.getUTCDate() + 4 - (copiedDate.getUTCDay() || 7)
+  );
+  var yearStart = new Date(Date.UTC(copiedDate.getUTCFullYear(), 0, 1));
+  var weekNo = Math.ceil(((copiedDate - yearStart) / 86400000 + 1) / 7);
+  return weekNo;
+};
 
 const now = new Date();
 
 const weekOfYear = getWeekNumber(now);
-console.log("Todays week number is "+weekOfYear);
-const statement = ""+year[weekOfYear];
+console.log("Todays week number is " + weekOfYear);
+const statement = "" + year[weekOfYear];
 
 console.log(statement);
 
@@ -40,4 +43,4 @@ const element = document.getElementById("who");
 element.innerText = statement;
 
 const dateElemenet = document.getElementById("date");
-dateElemenet.innerText = new Intl.DateTimeFormat('en-GB').format(now)
+dateElemenet.innerText = new Intl.DateTimeFormat("en-GB").format(now);
